79e34d2d91789b65c1394f28046801cdf6baefea
flyree <flyree@gmail.com>
2020-07-30 18:51:22 -0700

    Update fault.c

--- pFsysFI/fault.c
+++ pFsysFI/fault.c
@@ -20,8 +20,8 @@ void inject(Config *config, void *buf, size_t size){
 char* parepare_bitflip(void * buf, size_t size){
     char * buf_char = (char *)(buf);
     int inject_index = generate_random(size);
-    char data = buf_char[inject_index];
-    return &data;
+    char **data = &buf_char[inject_index];
+    return data;
 }
 
 int generate_random(size_t size){
@@ -29,13 +29,13 @@ int generate_random(size_t size){
     return rand()%size;
 }
 
-void bit_flip(char *data, int num_bits){
+void bit_flip(char **data, int num_bits){
     srand(time(NULL));
     log_msg("original: %c ",*data);
     size_t offset_start = rand()%(8-num_bits);
     int i = 0;
     for (i = 0; i < num_bits; i++){
-        *data ^= 1UL << offset_start+i;
+        **data ^= 1UL << offset_start+i;
     }
-    log_msg("after: %c ",*data);
+    log_msg("after: %c ",**data);
 }
