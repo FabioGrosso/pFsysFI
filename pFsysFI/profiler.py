28e7927820fce5ea0afb41d4bf0727772254c044
flyree <flyree@gmail.com>
2020-08-05 18:51:32 -0700

    add config and yaml loader

--- pFsysFI/profiler.py
+++ pFsysFI/profiler.py
@@ -4,10 +4,19 @@ import re
 import yaml
 
 OP_PREFIX = 'fifaa_'
-OP_MATCHING = OP_PREFIX+r'\(.*?\)'
-APP_NAME = "x"
+OP_MATCHING = OP_PREFIX+r'.*\(.*?\)'
+CONFIG = "config.yaml"
 
-def parse_log(logfile:str) -> dict:
+def config_loader(configfile:str):
+    yaml_file = open(configfile)
+    if yaml_file.closed:
+        logging.error("file is not open")
+        raise ValueError
+
+    parsed_yaml_loader = yaml.load(yaml_file, Loader=yaml.FullLoader)
+    return parsed_yaml_loader
+
+def parse_log(logfile:str,yaml_loader) -> dict:
 
     file_op = {}
     with open(logfile) as f:
@@ -21,14 +30,16 @@ def parse_log(logfile:str) -> dict:
                         file_op[op] = 1
                     else:
                         file_op[op] = file_op[op] + 1
-    gen_yaml(file_op,APP_NAME)
+    
+    app = yaml_loader["benchmark"]  
+    gen_yaml(file_op,app)
 
 def gen_yaml(op_list:dict,app:str):
 
     with open(app+".yaml", 'w') as y_file:
         documents = yaml.dump(op_list, y_file)
 
-
-parse_log("fifaa.log")
+yaml_loader = config_loader(CONFIG)
+parse_log("fifaa.log",yaml_loader)
 
 
