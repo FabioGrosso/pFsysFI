86556a726b2101e1a0c48c107daed916113c26f4
FabioGrosso <you@example.com>
2020-11-03 20:40:24 -0800

    fix fault.h bugs

--- pFsysFI/fault.h
+++ pFsysFI/fault.h
@@ -18,12 +18,13 @@ typedef struct Configuration{
 int generate_random(size_t size);
 
 
-void inject(Config config, void *buf, size_t size);
+int  inject(Config config, void *buf, size_t size);
 
 // for bitflip model
 char* parepare_bitflip(void * buf, size_t size);
 void bit_flip(char *data, int num_bits);
-
+char* prepare_bitflip(void * buf, size_t size);
+int prepare_shornwrite(void *buf, size_t size, float shorn_portion);
 int load_config(Config *config);
 
 #endif
